a2ce1c1597d316a661d895f34a7fe697aff2a642
bnx-inagaki <Hiroto_Inagaki@bandainamco-nexus.co.jp>
2025-11-19 09:34:21 +0900

    distもpush

---
+++ dist/components/embeds/YouTubeEmbed.d.ts
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { DivPropsWithoutRef } from 'react-html-props';
+import { YouTubeProps } from 'react-youtube';
+import { PlaceholderEmbedProps } from '../placeholder/PlaceholderEmbed';
+export interface YouTubeEmbedProps extends DivPropsWithoutRef {
+    url: string;
+    width?: string | number;
+    height?: string | number;
+    linkText?: string;
+    placeholderImageUrl?: string;
+    placeholderSpinner?: React.ReactNode;
+    placeholderSpinnerDisabled?: boolean;
+    placeholderProps?: PlaceholderEmbedProps;
+    embedPlaceholder?: React.ReactNode;
+    placeholderDisabled?: boolean;
+    youTubeProps?: YouTubeProps;
+    loadingTimeout?: number;
+    fallbackToThumbnail?: boolean;
+}
+export declare const YouTubeEmbed: ({ url, width, height, linkText, placeholderImageUrl, placeholderSpinner, placeholderSpinnerDisabled, placeholderProps, embedPlaceholder, placeholderDisabled, youTubeProps, loadingTimeout, fallbackToThumbnail, ...divProps }: YouTubeEmbedProps) => React.JSX.Element;
